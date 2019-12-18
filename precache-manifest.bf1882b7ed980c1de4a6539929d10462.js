0161176863463f4824aebd058b4b192c9e535ecf
Alexjzhh <wangyang.tx@gmail.com>
2019-12-18 16:23:51 -0700

    Updates

---
+++ precache-manifest.bf1882b7ed980c1de4a6539929d10462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f233ce3e65de917907ce09245decff67",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "6c3d35820e337ebaf18e",
+    "url": "/youtube-clone/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6c3d35820e337ebaf18e",
+    "url": "/youtube-clone/static/js/2.6d87633f.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/youtube-clone/static/js/2.6d87633f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2844aaca0698fbd2dc50",
+    "url": "/youtube-clone/static/js/main.538360ce.chunk.js"
+  },
+  {
+    "revision": "e73882c0308b1ea58b37",
+    "url": "/youtube-clone/static/js/runtime-main.801227af.js"
+  }
+]);
